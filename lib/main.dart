636dc42322e0fca84452c8a716ea9c7bf9aa2df2
agjohngabriel <agjohngabriel@gmail.com>
2021-09-19 15:58:38 -0400

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:find_me_flutter_app/screens/getstarted.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart'show kIsWeb;
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:intro_slider/dot_animation_enum.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+import 'package:intro_slider/scrollbar_behavior_enum.dart';
 import 'dart:io';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
     DeviceOrientation.portraitUp,
@@ -19,18 +23,136 @@ class MyApp extends StatelessWidget {
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.dark,
       statusBarBrightness:
-      !kIsWeb && Platform.isAndroid ? Brightness.dark : Brightness.light,
+          !kIsWeb && Platform.isAndroid ? Brightness.dark : Brightness.light,
       systemNavigationBarColor: Colors.white,
       systemNavigationBarDividerColor: Colors.transparent,
       systemNavigationBarIconBrightness: Brightness.dark,
     ));
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: GetStartedScreen()
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: IntroScreen());
   }
 }
 
+class IntroScreen extends StatefulWidget {
+  IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  IntroScreenState createState() => new IntroScreenState();
+}
+
+// ------------------ Custom config ------------------
+class IntroScreenState extends State<IntroScreen> {
+  List<Slide> slides = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides.add(
+      new Slide(
+        backgroundImage: "assets/find_me_splash1.png",
+        directionColorBegin: Alignment.topLeft,
+        directionColorEnd: Alignment.bottomRight,
+        onCenterItemPress: () {},
+      ),
+    );
+    slides.add(
+      new Slide(
+        backgroundImage: "assets/find_me_splash2.png",
+        directionColorBegin: Alignment.topRight,
+        directionColorEnd: Alignment.bottomLeft,
+      ),
+    );
+    slides.add(
+      new Slide(
+        backgroundImage: "assets/find_me_splash3.png",
+        directionColorBegin: Alignment.topCenter,
+        directionColorEnd: Alignment.bottomCenter,
+      ),
+    );
+    slides.add(
+      new Slide(
+        backgroundImage: "assets/find_me_splash4.png",
+        directionColorBegin: Alignment.topCenter,
+        directionColorEnd: Alignment.bottomCenter,
+      ),
+    );
+    slides.add(
+      new Slide(
+        backgroundImage: "assets/find_me_splash5.png",
+        directionColorBegin: Alignment.topCenter,
+        directionColorEnd: Alignment.bottomCenter,
+      ),
+    );
+    slides.add(
+      new Slide(
+        backgroundImage: "assets/find_me_splash6.png",
+        directionColorBegin: Alignment.topCenter,
+        directionColorEnd: Alignment.bottomCenter,
+      ),
+    );
+  }
+
+  void onDonePress() {
+    // Do what you want
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => GetStartedScreen()),
+    );
+  }
+
+  Widget renderDoneBtn() {
+    return Icon(
+      Icons.arrow_forward,
+      color: Color(0xffF3B4BA),
+    );
+  }
+
+  ButtonStyle myButtonStyle() {
+    return ButtonStyle(
+      shape: MaterialStateProperty.all<OutlinedBorder>(StadiumBorder()),
+      backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF2B365A)),
+      overlayColor: MaterialStateProperty.all<Color>(Color(0x33FFA8B0)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness:
+          !kIsWeb && Platform.isAndroid ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return new IntroSlider(
+      // List slides
+      slides: this.slides,
+      // Done button
+      showSkipBtn: false,
+      showPrevBtn: false,
+      showNextBtn: false,
+      renderDoneBtn: this.renderDoneBtn(),
+      onDonePress: this.onDonePress,
+      doneButtonStyle: myButtonStyle(),
+
+      // Dot indicator
+      colorDot: Color(0xFFC4C4C4),
+      colorActiveDot: Color(0xFF2B365A),
+      sizeDot: 10.0,
+
+      // Show or hide status bar
+      hideStatusBar: false,
+      backgroundColorAllSlides: Colors.white,
+
+      // Scrollbar
+      verticalScrollbarBehavior: scrollbarBehavior.SHOW_ALWAYS,
+    );
+  }
+}
--- lib/screens/getstarted.dart
+++ lib/screens/getstarted.dart
@@ -16,7 +16,7 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      top: false,
+        top: false,
         bottom: false,
         child: Scaffold(
           appBar: AppBar(
@@ -27,21 +27,31 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
           backgroundColor: AppTheme.nearlyWhite,
           body: Column(
             children: [
-                    SizedBox(height: 20),
-                    Align(
-                      alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 25.0),
-                          child: Text("Lets get started", style: AppTheme.headerTextStyle,),
-                        )),
-                    SizedBox(height: 5,),
-                    Align(
-                      alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 25.0),
-                          child: Text("Select your account to sign in", style: AppTheme.greyTextStyle,),
-                        )),
-              SizedBox(height: 40,),
+              SizedBox(height: 20),
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Text(
+                      "Lets get started",
+                      style: AppTheme.headerTextStyle,
+                    ),
+                  )),
+              SizedBox(
+                height: 5,
+              ),
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 25.0),
+                    child: Text(
+                      "Select your account to sign in",
+                      style: AppTheme.greyTextStyle,
+                    ),
+                  )),
+              SizedBox(
+                height: 40,
+              ),
               Container(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -50,75 +60,101 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
                       children: [
                         Container(
                           height: MediaQuery.of(context).size.height / 3,
-                          width: MediaQuery.of(context).size.height /7,
+                          width: MediaQuery.of(context).size.height / 4,
                           decoration: BoxDecoration(
-                            color: AppTheme.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
+                              color: AppTheme.white,
+                              borderRadius: BorderRadius.circular(16),
+                              image: DecorationImage(
+                                  image: AssetImage('assets/vendor.png'),
+                                  fit: BoxFit.cover)),
                           child: Align(
-                            alignment: Alignment.bottomLeft,
+                              alignment: Alignment.bottomLeft,
                               child: Padding(
-                                padding: const EdgeInsets.only(left: 18.0,bottom: 20),
-                                child: Text('User', style: AppTheme.whiteHeaderTextStyle,),
+                                padding: const EdgeInsets.only(
+                                    left: 8.0, bottom: 18),
+                                child: Text(
+                                  'User',
+                                  style: AppTheme.whiteHeaderTextStyle,
+                                ),
                               )),
                         ),
-                        SizedBox(height: 30,),
                         TextButton(
-                          onPressed: (){},
-                          child: Text('Sign up as user', style: AppTheme.greyTextStyle),
-                        )
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        CreateAccount()));
+                          },
+                          child: Text('Sign up as user',
+                              style: AppTheme.greyTextStyle),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
                       ],
                     ),
                     Column(
                       children: [
                         Container(
-                          height: MediaQuery.of(context).size.height / 3,
-                          width: MediaQuery.of(context).size.height /4,
+                          height: MediaQuery.of(context).size.height / 4,
+                          width: MediaQuery.of(context).size.height / 7,
                           decoration: BoxDecoration(
                               color: AppTheme.nearlyWhite,
-                              borderRadius: BorderRadius.circular(16),
+                              borderRadius: BorderRadius.circular(10),
                               image: DecorationImage(
-                                  image: AssetImage(
-                                      'assets/vendor.png'
-                                  ),
-                                  fit: BoxFit.cover
-                              )
-                          ),
+                                  image: AssetImage('assets/vendor.png'),
+                                  fit: BoxFit.cover)),
                           child: Align(
                             alignment: Alignment.bottomLeft,
                             child: Stack(
                               children: [
                                 Padding(
-                                  padding: const EdgeInsets.only(bottom: 8.0, left: 18),
-                                  child: Text('Vendor', style: AppTheme.whiteHeaderTextStyle,),
+                                  padding: const EdgeInsets.only(
+                                      bottom: 8.0, left: 18),
+                                  child: Text(
+                                    'Vendor',
+                                    style: AppTheme.whiteHeaderTextStyle,
+                                  ),
                                 ),
                               ],
                             ),
                           ),
                         ),
-
                         TextButton(
-                          onPressed: (){},
-                          child: Text('Sign up as vendor', style: AppTheme.greyTextStyle),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        CreateAccount()));
+                          },
+                          child: Text('Sign up as vendor',
+                              style: AppTheme.greyTextStyle),
                         )
                       ],
                     )
                   ],
                 ),
               ),
-              SizedBox(height: 50,),
+              SizedBox(
+                height: 50,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 28.0),
                 child: CustomButton(
-                  decorationColor: AppTheme.blue,
+                    decorationColor: AppTheme.blue,
                     buttonHeight: 48,
                     borderColor: AppTheme.blue,
                     buttonRadius: 10,
                     buttonText: ('Login'),
-                    onPressed: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=>CreateAccount()));
-                    }
-                ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  CreateAccount()));
+                    }),
               ),
               SizedBox(
                 height: 10,
@@ -133,7 +169,10 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
                       width: MediaQuery.of(context).size.width / 2.8,
                       color: AppTheme.blue.withOpacity(0.6),
                     ),
-                    Text('Or', style: AppTheme.blackTextStyle,),
+                    Text(
+                      'Or',
+                      style: AppTheme.blackTextStyle,
+                    ),
                     Container(
                       height: 1.5,
                       width: MediaQuery.of(context).size.width / 2.8,
@@ -142,7 +181,9 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
                   ],
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 12.0),
                 child: Row(
@@ -150,55 +191,57 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
                   children: [
                     CustomIconButton(
                       buttonImage: Image.asset('assets/google.png'),
-                        buttonWidth: MediaQuery.of(context).size.width / 4.5,
-                        buttonRadius: 10,
-                        onPressed: (){
-
-                        },
+                      buttonWidth: MediaQuery.of(context).size.width / 4.5,
+                      buttonRadius: 10,
+                      onPressed: () {},
                       buttonHeight: 45,
                     ),
                     CustomIconButton(
                       buttonImage: Image.asset('assets/apple.png'),
-                        buttonWidth: MediaQuery.of(context).size.width / 4.5,
-                        buttonRadius: 10,
-                        onPressed: (){
-
-                        },
+                      buttonWidth: MediaQuery.of(context).size.width / 4.5,
+                      buttonRadius: 10,
+                      onPressed: () {},
                       buttonHeight: 45,
                     ),
                     CustomIconButton(
                       buttonImage: Image.asset('assets/facebook.png'),
-                        buttonWidth: MediaQuery.of(context).size.width / 4.5,
-                        buttonRadius: 10,
-                        onPressed: (){
-
-                        },
+                      buttonWidth: MediaQuery.of(context).size.width / 4.5,
+                      buttonRadius: 10,
+                      onPressed: () {},
                       buttonHeight: 45,
                     ),
                   ],
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 85.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text('Sign up as a', style: TextStyle(
-                        fontWeight: FontWeight.w400, fontSize: 15, fontFamily: 'Poppins', color: AppTheme.blue
-                    )),
+                    Text('Sign up as a',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 15,
+                            fontFamily: 'Poppins',
+                            color: AppTheme.blue)),
                     InkWell(
-                      onTap: (){},
-                      child: Text('Findme Agent?', style: TextStyle(
-                          fontWeight: FontWeight.w600, fontSize: 16, fontFamily: 'Poppins', color: AppTheme.blue, decoration: TextDecoration.underline
-                      )),
+                      onTap: () {},
+                      child: Text('Findme Agent?',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                              fontFamily: 'Poppins',
+                              color: AppTheme.blue,
+                              decoration: TextDecoration.underline)),
                     ),
                   ],
                 ),
               )
             ],
           ),
-        )
-    );
+        ));
   }
 }
